2aa7ff7f22d37b99ca069bc46f1b68c3e36793af
Ivan <ivan@spasoye.com>
2024-02-10 16:51:42 +0100

    implemented C++ support

---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/sys/printk.h>
+#include <inttypes.h>
+#include "../inc/mag_sens.h"
+
+#define SLEEP_TIME_MS	1
+
+/*
+ * Get button configuration from the devicetree sw0 alias. This is mandatory.
+ */
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios,
+							      {0});
+static struct gpio_callback button_cb_data;
+
+/*
+ * The led0 devicetree alias is optional. If present, we'll use it
+ * to turn on the LED whenever the button is pressed.
+ */
+static struct gpio_dt_spec led = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios,
+						     {0});
+
+void button_pressed(const struct device *dev, struct gpio_callback *cb,
+		    uint32_t pins)
+{
+	// gpio_pin_interrupt_configure_dt(&button, GPIO_INT_DISABLE);
+	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
+}
+
+int main(void)
+{
+	mag_sense speed_meter = mag_sense::mag_sense;
+
+	printk("TEST Press the button\n");
+
+	if (led.port) {
+		while (1) {
+			/* If we have an LED, match its state to the button's. */
+			int val = gpio_pin_get_dt(&button);
+
+			if (val >= 0) {
+				gpio_pin_set_dt(&led, val);
+			}
+			k_msleep(SLEEP_TIME_MS);
+		}
+	}
+	return 0;
+}
