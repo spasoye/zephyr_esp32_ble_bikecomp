e3737b3bf5556dae3d476bde18acd53b33ac1846
Ivan <ivan@spasoye.com>
2024-02-18 23:08:40 +0100

    bluetooth func test

--- src/main.cpp
+++ src/main.cpp
@@ -5,67 +5,61 @@
 #include <zephyr/sys/printk.h>
 #include <inttypes.h>
 #include "../inc/mag_sens.h"
+#include "../inc/ble_csc_app.h"
 
-#define SLEEP_TIME_MS	500
+// #define SLEEP_TIME_MS	500
 
-#define SW0_NODE	DT_ALIAS(magsw)
-#if !DT_NODE_HAS_STATUS(SW0_NODE, okay)
-#error "Unsupported board: sw0 devicetree alias is not defined"
-#endif
+// #define SW0_NODE	DT_ALIAS(magsw)
+// #if !DT_NODE_HAS_STATUS(SW0_NODE, okay)
+// #error "Unsupported board: sw0 devicetree alias is not defined"
+// #endif
 
-/*
- * Get button configuration from the devicetree sw0 alias. This is mandatory.
- */
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios,
-							      {0});
-static struct gpio_callback button_cb_data;
+// /*
+//  * Get button configuration from the devicetree sw0 alias. This is mandatory.
+//  */
+// static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios,
+// 							      {0});
+// static struct gpio_callback button_cb_data;
 
-/*
- * The led0 devicetree alias is optional. If present, we'll use it
- * to turn on the LED whenever the button is pressed.
- */
-static struct gpio_dt_spec led = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios,
-						     {0});
+// /*
+//  * The led0 devicetree alias is optional. If present, we'll use it
+//  * to turn on the LED whenever the button is pressed.
+//  */
+// static struct gpio_dt_spec led = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios,
+// 						     {0});
 
-void button_pressed(const struct device *dev, struct gpio_callback *cb,
-		    uint32_t pins)
-{
-	// gpio_pin_interrupt_configure_dt(&button, GPIO_INT_DISABLE);
-	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
-}
+// void button_pressed(const struct device *dev, struct gpio_callback *cb,
+// 		    uint32_t pins)
+// {
+// 	// gpio_pin_interrupt_configure_dt(&button, GPIO_INT_DISABLE);
+// 	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
+// }
 
 int main(void)
 {
-	int ret;
+	int err;
+
+	// Enable Bluetooth
+	err = bt_enable(NULL);
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return 0;
+	}
+
+	// Callback when Bluetooth is ready
+	bt_ready();
+
+	while (1) {
+		// Sleep for 1 second
+		k_sleep(K_SECONDS(1));
+
+
+		csc_simulation();
+
+
+		// Simulate battery level notification
+		bas_notify();
+	}
 	
-	// Magnetic switch testing
-	mag_sens a(&button);
-
-	if (led.port && !gpio_is_ready_dt(&led)) {
-		printk("Error %d: LED device %s is not ready; ignoring it\n",
-		       ret, led.port->name);
-		led.port = NULL;
-	}
-	if (led.port) {
-		ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT);
-		if (ret != 0) {
-			printk("Error %d: failed to configure LED device %s pin %d\n",
-			       ret, led.port->name, led.pin);
-			led.port = NULL;
-		} else {
-			printk("Set up LED at %s pin %d\n", led.port->name, led.pin);
-		}
-	}
-
-	printk("TEST Press the button\n");
-
-	if (led.port) {
-		while (1) {
-			static bool val=true;
-			val = !val;
-			gpio_pin_set_dt(&led, val);
-			k_msleep(SLEEP_TIME_MS);
-		}
-	}
 	return 0;
 }
